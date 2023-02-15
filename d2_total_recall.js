ed589176a6e552d5c4cf1b4378409ba8a44919dc
Mayleen <mayleennyc@gmail.com>
2023-02-15 13:21:59 -0500

    Fizz Buzz answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -8,20 +8,29 @@
 ////////////////////////////////
 
 //* Get Even
-for (let i = 0; i<=200; i+=1){
-    if (i % 2 === 0){
-        console.log((`${i} is even`))
-    } else {
-        continue;
+// for (let i = 0; i<=200; i+=1){
+//     if (i % 2 === 0){
+//         console.log((`${i} is even`))
+//     } else {
+//         continue;
+//     }
+// }
+
+////////////////////////////////
+
+//* Fizz Buzz
+for (let i = 1; i<=100; i++){
+    if (i % 3 === 0 && i % 5 ==0){
+        console.log(`FizzBuzz`)
+    } else if (i % 3 === 0){
+        console.log('Fizz')
+    } else if (i % 5 === 0){
+        console.log('Buzz')
     }
 }
 
 
-////////////////////////////////
 
-
-////////////////////////////////
-//* Fizz Buzz
 ////////////////////////////////
 
 
